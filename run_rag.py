db18f51058c60e0fa87b72dca41c990f26c103b1
emmanuelol <emmanuelort@gmail.com>
2025-02-14 18:49:04 -0600

    gradio working

--- run_rag.py
+++ run_rag.py
@@ -10,11 +10,11 @@ from langchain.vectorstores import Chroma
 from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain_ollama import ChatOllama
 from ollama import ChatResponse, chat, Client
-import ollama
+#import ollama
 import re
 import os
 import yaml
-
+from time import sleep
 
 
 def process_pdf(pdf_bytes, model_embedding, persist_directory,chunk_size,chunk_overlap):
@@ -39,6 +39,7 @@ def process_pdf(pdf_bytes, model_embedding, persist_directory,chunk_size,chunk_o
 
 def load_embeddings(model_embedding, persist_directory, chunk_size, chunk_overlap):
     base_url = 'http://ollama:11434'
+    print(model_embedding)
     vectordb = Chroma(persist_directory=persist_directory,
                       embedding_function= OllamaEmbeddings(model=model_embedding,base_url=base_url))
     
@@ -46,7 +47,7 @@ def load_embeddings(model_embedding, persist_directory, chunk_size, chunk_overla
          chunk_size=chunk_size, 
          chunk_overlap=chunk_overlap
     )
-    
+   
     retriever = vectordb.as_retriever()
 
     
@@ -86,7 +87,7 @@ def rag_chain(question, text_splitter, retriever, model_embedding):
 
 def ask_question(pdf_bytes, question, create_embeddings, embeddings_directory, model_embedding, chunk_size, chunk_overlap):
     if create_embeddings is True:
-        print(create_embeddings)
+        print('create_embeddings')
         text_splitter, vectorstore, retriever = process_pdf(pdf_bytes,
                                                             model_embedding,
                                                             embeddings_directory,
@@ -94,6 +95,7 @@ def ask_question(pdf_bytes, question, create_embeddings, embeddings_directory, m
                                                             chunk_overlap
                                                            )
     else:
+        print('preloaded')
         text_splitter, vectorstore, retriever = load_embeddings(model_embedding,
                                                                 embeddings_directory,
                                                                 chunk_size,
@@ -102,7 +104,7 @@ def ask_question(pdf_bytes, question, create_embeddings, embeddings_directory, m
 
     if embeddings_directory is None:
         return None  # No PDF uploaded
-
+    print(retriever)
     result = rag_chain(question, 
                        text_splitter,  
                        retriever, 
@@ -111,45 +113,58 @@ def ask_question(pdf_bytes, question, create_embeddings, embeddings_directory, m
     #return {result}
     return result
 
-
 def main():
-    # Load parameters
-    with open('config.yaml','r') as file:
-        config = yaml.safe_load(file)
 
-    model_embedding = config['models']['embedding']
+    # Load parameters
+    #with open('config.yaml','r') as file:
+    #    config = yaml.safe_load(file)
+
+#    model_embedding = config['models']['embedding']
 #    print(model_embedding)
-    embeddings_directory = config['path']['emmbeddings']
+#    embeddings_directory = config['path']['emmbeddings']
  #   print(embeddings_directory)
-    pdf_bytes = config['path']['pdfs']
+#    pdf_bytes = config['path']['pdfs']
  #   print(pdf_bytes)
-    create_embeddings = config['parameters']['create_embeddings']
+#    create_embeddings = config['parameters']['create_embeddings']
   #  print(create_embeddings)
-    chunk_size = config['parameters']['chunk_size']
+#    chunk_size = config['parameters']['chunk_size']
    # print(chunk_size)
-    chunk_overlap = config['parameters']['chunk_overlap']
+#    chunk_overlap = config['parameters']['chunk_overlap']
     #print(chunk_overlap)
-    question ='please provide me the structure of a docker-compose.yaml file'
+#    question ='please provide me the structure of a docker-compose.yaml file'
    # print(question)
-    results = ask_question(pdf_bytes ,question,create_embeddings, embeddings_directory, model_embedding, chunk_size, chunk_overlap)
-    print(results)
-'''
+    #results = ask_question(pdf_bytes ,question,create_embeddings, embeddings_directory, model_embedding, chunk_size, chunk_overlap)
+    #print(results)
+    ###
+    # path_to_pdf,question,
+
+    print('loading')
+    #sleep(5*60)
+
     interface = gr.Interface(
         fn=ask_question,
         inputs=[
-            gr.File(label="Path to upload PDF directory (optional)"),
-            gr.Textbox(label="Ask a question"),
-            gr.Checkbox(label='Preloaded', info='Preloaded embeddings'),
-            gr.File(label='/datasets/')
+            gr.File(label="Path to upload PDF directory (optional)"), # path to pdf
+            gr.Textbox(label="Ask a question"), # question
+            gr.Checkbox(value=False,label='create embeddings', info='create embeddings'), # check to create embeddings
+            #gr.File(label='Path to embeddings'), # path of the embeddings
+            gr.Textbox(value='/datasets/deepseek-r1',label='Path to embeddings'), # path of the embeddings
+            gr.Dropdown(['deepseek-r1:1.5b','deepseel-r1:7b'],label='model name'), ## model_name
+            gr.Slider(1,1000,value=500,label='chunk size'), ## chunk_size
+            gr.Slider(1,200,value=100,label='chunk overlap'), ## chuck_overlap
 
         ],
-        outputs="text",
-        title="Ask questions about your PDF",
+        outputs="textbox",
+        title="Ask questions about Machine Learning, data science and, MLOps based on Packt PDFs",
         description="Use DeepSeek-R1 to answer your questions about the uploaded PDF documents.",
     )
 
+    interface.launch(share=True)
+    #interface.launch(inbrowser=True)
+'''
 if __name__ == '__main__':
     interface.launch()
+
 '''
 if __name__ == '__main__':
     main()
